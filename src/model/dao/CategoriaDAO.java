7a48d22730a8bab51718831fa4c65f6b05f2d345
Fabricio <Fabricio@DESKTOP-5CHIT5S>
2022-11-24 20:30:30 +0200

    -Implementacao do metodo remover na classe categoria da interface CRUD. -Implementacao do metodo selecionar na classe categoria da interface CRUD.

--- src/model/dao/CategoriaDAO.java
+++ src/model/dao/CategoriaDAO.java
@@ -8,7 +8,9 @@ package model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import model.Conexao;
 import static model.dao.BancoDeDados.*;
@@ -57,12 +59,38 @@ public class CategoriaDAO implements CRUD<Categoria>{
     }
 
     @Override
-    public void remover(Categoria e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void remover(Categoria categoria) {
+        String query = "delete from categoria where codigo = ?";
+        try{
+            Connection conexao = new Conexao(HOST, NOME_BANCO_DE_DADOS, USUARIO, "").getConexao();    
+            PreparedStatement preparedStatement = conexao.prepareStatement(query);
+            preparedStatement.setString(1, categoria.getCodigo());
+            preparedStatement.execute();
+            conexao.close();
+        }catch(SQLException se){
+            System.out.println("erro ao tentar remover categoria: " + se.getMessage());
+        }
     }
 
     @Override
     public List<Categoria> selecionar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      ArrayList<Categoria> categorias = new ArrayList<>();
+      String query = "select * from categoria";
+      
+      try{
+            Connection conexao = new Conexao(HOST, NOME_BANCO_DE_DADOS, USUARIO, "").getConexao();    
+            PreparedStatement preparedStatement = conexao.prepareStatement(query);
+            ResultSet result = preparedStatement.executeQuery();
+            while(result.next()){
+                String codigo = result.getString("codigo");
+                String nome = result.getString("nome");
+                Categoria categoria = new Categoria(codigo, nome);
+                categorias.add(categoria);
+            }
+            conexao.close();
+      }catch(SQLException se){
+          System.out.println("erro ao tentar selecionar categorias: "+se.getMessage());
+      }
+      return categorias;
     }
 }
