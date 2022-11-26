69706e5af2b35c99826824264dc10579f7223d65
Fabricio <Fabricio@DESKTOP-5CHIT5S>
2022-11-26 14:19:51 +0200

    Implementacao dos metodos da interface dao.CRUD na classe ClienteDAO

--- src/model/dao/ClienteDAO.java
+++ src/model/dao/ClienteDAO.java
@@ -5,7 +5,16 @@
  */
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import model.Conexao;
+import static model.dao.BancoDeDados.HOST;
+import static model.dao.BancoDeDados.NOME_BANCO_DE_DADOS;
+import static model.dao.BancoDeDados.USUARIO;
 import model.vo.Cliente;
 
 /**
@@ -15,18 +24,72 @@ import model.vo.Cliente;
 public class ClienteDAO implements CRUD<Cliente>{
 
     @Override
-    public void inserir(Cliente e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void inserir(Cliente cliente) {
+       String codigo = cliente.getCodigo();
+       String nome = cliente.getNome();
+       String endereco = cliente.getEndereco();
+       String telefone = cliente.getTelefone();
+       
+       String query = "INSERT INTO Cliente (codigo, nome, endereco, telefone)"
+               + "VALUES (?, ?, ?, ?)";   
+       
+       try{
+          Connection conexao = new Conexao(HOST, NOME_BANCO_DE_DADOS, USUARIO, "").getConexao();  
+          PreparedStatement preparedStatement  = conexao.prepareStatement(query);
+          
+          preparedStatement.setString(1, codigo);
+          preparedStatement.setString(2, nome);
+          preparedStatement.setString(3, endereco);
+          preparedStatement.setString(4, telefone);
+          
+          preparedStatement.executeUpdate();
+          
+          conexao.close();
+       }catch(SQLException se){
+           System.out.println("erro ao tentar inserir cliente: " + se.getMessage());
+       }
     }
 
     @Override
     public void remover(Cliente e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       String query = "DELETE FROM CLIENTE WHERE codigo = ?";
+       
+       try{
+           Connection conexao = new Conexao(HOST, NOME_BANCO_DE_DADOS, USUARIO, "").getConexao();  
+           PreparedStatement preparedStatement  = conexao.prepareStatement(query);
+       } catch (SQLException se) {
+           System.out.println("erro ao tentar remover cliente: " + se.getMessage());
+        }
     }
 
     @Override
     public List<Cliente> selecionar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList clientes = new ArrayList();
+        String query = "SELECT * FROM Cliente";
+        
+        try{
+            Connection conexao = new Conexao(HOST, NOME_BANCO_DE_DADOS, USUARIO, "").getConexao();  
+            PreparedStatement preparedStatement  = conexao.prepareStatement(query);
+            
+            ResultSet result = preparedStatement.executeQuery();
+            
+            while(result.next()){
+                String codigo = result.getString("codigo");
+                String nome = result.getString("nome");
+                String telefone = result.getString("telefone");
+                String endereco = result.getString("endereco");
+                
+                Cliente cliente = new Cliente(codigo, endereco, nome, telefone);
+                
+                clientes.add(cliente);
+            }
+            
+            conexao.close();
+        } catch (SQLException se) {
+            System.out.println("erro ao tentar selecionar os clientes");
+        }
+        
+        return clientes;
     }
     
 }
