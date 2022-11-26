e46d98aad2e5433878c10608eab2d3c6f04e9b87
Fabricio <Fabricio@DESKTOP-5CHIT5S>
2022-11-26 20:55:49 +0200

    Implementacao de metodos inserir, remover obterCategorias (os nomes) e nomeCodigo (um mapa de nome para codigo) para categoriaController

--- src/controller/CategoriaController.java
+++ src/controller/CategoriaController.java
@@ -5,12 +5,48 @@
  */
 package controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.dao.CategoriaDAO;
+import model.vo.Categoria;
+
 /**
  *
  * @author Fabricio
  */
 public class CategoriaController {
-    public void inserir(String nome){
+    CategoriaDAO categoriaDAO;
+    
+    public CategoriaController(){
+        categoriaDAO = new CategoriaDAO();
+    }
+    
+    public boolean inserir(String nome){
+        Categoria categoria = new Categoria(0, nome); //O codigo zero nao interessa a o codigo vai ser autogerado no banco de dados
+        return categoriaDAO.inserir(categoria);
+    }
+    
+    public boolean remover(int codigo){
+        return categoriaDAO.remover(new Categoria(codigo, ""));
+    }
+    
+    public List<String> obterCategorias(){
+        ArrayList<String> categorias = new ArrayList();
+        categoriaDAO.selecionar().forEach((categoria) -> {
+            categorias.add(categoria.getNome());
+        }); 
+        return categorias;
+    }
+    
+    public Map<String, Integer> MapaNomeCodigo(){
+        Map<String, Integer> codigos = new HashMap(); 
         
+        categoriaDAO.selecionar().forEach((categoria) -> {
+            codigos.put(categoria.getNome(), categoria.getCodigo());
+        });
+        
+        return codigos;
     }
 }
