40e928a3b740fe6f0d4b112038d2797e0e71afc1
Fabricio <Fabricio@DESKTOP-5CHIT5S>
2022-11-26 21:11:34 +0200

    Adicao do metodo inserir e remover em EmpregadoController

--- src/controller/EmpregadoController.java
+++ src/controller/EmpregadoController.java
@@ -5,10 +5,27 @@
  */
 package controller;
 
+import controller.Enumeracoes.Sexo;
+import controller.Enumeracoes.TipoDeEmpregado;
+import model.dao.EmpregadoDAO;
+import model.vo.Empregado;
+
 /**
  *
  * @author Fabricio
  */
 public class EmpregadoController {
+    private EmpregadoDAO empregadoDAO;
+    public EmpregadoController(){
+        empregadoDAO = new EmpregadoDAO();
+    }
     
+    public boolean inserir(String codigo, String nome, Sexo sexo, String telefone, String endereco, String senha, TipoDeEmpregado tipo){
+        Empregado empregado = new Empregado(codigo, endereco, nome, senha, sexo, telefone, tipo);
+        return empregadoDAO.inserir(empregado);
+    }
+    
+    public boolean remover(String codigo){
+        return empregadoDAO.remover(new Empregado(codigo, "", "", "", Sexo.M, "", TipoDeEmpregado.ATENDENTE));//Somente o codigo interessa
+    }
 }
