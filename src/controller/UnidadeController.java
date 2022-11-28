b559c37af3d6f15f024ca08ae43280f94cb4be9f
Fabricio <Fabricio@DESKTOP-5CHIT5S>
2022-11-28 22:12:54 +0200

    Adicao do metodo atualizar(Cliente cliente) que atualiza os dados do cliente (Este nao deve ser usado na view uma versao para tal sera fornecida mais tarde)

--- src/controller/UnidadeController.java
+++ src/controller/UnidadeController.java
@@ -7,6 +7,7 @@ package controller;
 
 import controller.Enumeracoes.Disponibilidade;
 import controller.Enumeracoes.EstadoDeConservacao;
+import java.util.List;
 import model.dao.UnidadeDAO;
 import model.vo.Unidade;
 
@@ -16,16 +17,71 @@ import model.vo.Unidade;
  */
 public class UnidadeController {
     UnidadeDAO unidadeDAO;
+    private static List<Unidade> unidades;
+    private static List<Unidade> unidadesDisponiveis;
+    private List<Unidade> unidadesIndisponiveis;
+    private static long count = 0;
+    
+    
     public UnidadeController(){
         unidadeDAO = new UnidadeDAO();
+        count = 1;
+        atualizarLista();
+    }
+    
+    public void atualizarLista(){
+        if(count > 0){
+            unidades = unidadeDAO.selecionar();
+            unidades.forEach((unidade) -> {
+                if(unidade.getDisponibilidade().toString().equalsIgnoreCase(Disponibilidade.DISPONIVEL.toString())){
+                    unidadesDisponiveis.add(unidade);
+                }else{
+                    unidadesIndisponiveis.add(unidade);
+                }
+            });
+        }
+    }
+    
+    public boolean atualizar(Unidade unidade){
+        count++;
+        return unidadeDAO.atualizar(unidade);
     }
     
     public boolean inserir(int codigo, int codigoMaterial, Disponibilidade disponibilidade, EstadoDeConservacao estadaConservacao){
+        count++;
         Unidade unidade = new Unidade(codigo, codigoMaterial, disponibilidade, estadaConservacao);
         return unidadeDAO.inserir(unidade);
     }
     
     public boolean remover(int codigo){
+        count++;
         return unidadeDAO.remover(new Unidade(codigo, 0, Disponibilidade.DISPONIVEL, EstadoDeConservacao.CONSERVADO));
     }
+    
+    public List<Unidade> getUnidades(){
+        return unidades;
+    }
+    
+    public List<Unidade> getUnidadesDisponiveis(){
+        return unidadesDisponiveis;
+    }
+    
+    public List<Unidade> getUnidadesIndiponiveis(){
+        return unidadesIndisponiveis;
+    }
+    
+    public void alocarNaListaDevida(Unidade unidade){
+        if(unidades.contains(unidade)){
+            if(unidade.getDisponibilidade().toString().equalsIgnoreCase(Disponibilidade.DISPONIVEL.toString())){
+                unidadesDisponiveis.remove(unidade); //So para evitar duplicatas (remover se exister)
+                unidadesDisponiveis.add(unidade); //(Reinserir, nesta caso sem duplicata)
+                unidadesIndisponiveis.remove(unidade);
+            }else{
+                unidadesIndisponiveis.remove(unidade); //So para evitar duplicatas (remover se exister)
+                unidadesIndisponiveis.add(unidade); //(Reinserir, nesta caso sem duplicata)
+                unidadesDisponiveis.remove(unidade);
+            }
+        }
+    }
+    
 }
