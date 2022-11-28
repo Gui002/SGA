831b6bbdc37d9b1d22af22b0447bb47a66ffa989
Fabricio <Fabricio@DESKTOP-5CHIT5S>
2022-11-28 23:31:37 +0200

    Adicao dos metodos getCodigos que retorna a lista de todos empregados disponiveis
    Adicao do metodo getEmpregado que retorna um Map com dados de um empregado cujo codigo eh passado por parametro. As chaves sao os atributos usados no banco de dados para a entidade empregado e os valores sao os valores correspondentes no banco de dados

--- src/controller/EmpregadoController.java
+++ src/controller/EmpregadoController.java
@@ -7,8 +7,13 @@ package controller;
 
 import controller.Enumeracoes.Sexo;
 import controller.Enumeracoes.TipoDeEmpregado;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import model.dao.EmpregadoDAO;
 import model.vo.Empregado;
+import utilitarios.Coleccoes;
 
 /**
  *
@@ -16,16 +21,57 @@ import model.vo.Empregado;
  */
 public class EmpregadoController {
     private EmpregadoDAO empregadoDAO;
+    private static List<Empregado> empregados;
+    private static long count = 0;
+    
     public EmpregadoController(){
         empregadoDAO = new EmpregadoDAO();
+        count = 1;
+        atualizar();
+    }
+    
+    private void atualizar(){
+        if(count > 0){
+            empregados = empregadoDAO.selecionar();
+            count = 0;
+        }
     }
     
     public boolean inserir(String codigo, String nome, Sexo sexo, String telefone, String endereco, String senha, TipoDeEmpregado tipo){
+        count ++;
         Empregado empregado = new Empregado(codigo, endereco, nome, senha, sexo, telefone, tipo);
         return empregadoDAO.inserir(empregado);
     }
     
     public boolean remover(String codigo){
+        count++;
         return empregadoDAO.remover(new Empregado(codigo, "", "", "", Sexo.M, "", TipoDeEmpregado.ATENDENTE));//Somente o codigo interessa
+    }
+    
+    public List<String> getCodigos(){
+        atualizar();
+        List<String> codigos = new ArrayList();
+        empregados.forEach((empregado) -> {
+            codigos.add(empregado.getCodigo());
+        });
+        
+        return codigos;
+    }
+    
+    public Map<String, Object> getEmpregado(String codigo){
+        Map<String, Object> result = new HashMap();
+        Empregado empregado = Coleccoes.achar(empregados, (Empregado e) -> {
+            return e.getCodigo().equalsIgnoreCase(codigo);
+        });
+        
+        result.put("codigo",   (empregado == null) ? "": empregado.getCodigo());
+        result.put("nome",     (empregado == null) ? "": empregado.getNome());
+        result.put("sexo",     (empregado == null) ? "": empregado.getSexo());
+        result.put("senha",    (empregado == null) ? "": empregado.getSenha());
+        result.put("telefone", (empregado == null) ? "": empregado.getTelefone());
+        result.put("endereco", (empregado == null) ? "": empregado.getEndereco());
+        result.put("tipo",     (empregado == null) ? "": empregado.getTipo());
+        
+        return result;
     }
 }
