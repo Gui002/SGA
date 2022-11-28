340a76cfddbc6763fbd53d7a6015fe577bf5a597
Fabricio <Fabricio@DESKTOP-5CHIT5S>
2022-11-28 22:08:34 +0200

    Adicao de metodo para adicionar um novo aluguel novoAluguel, usar a versao publica

--- src/controller/AluguelController.java
+++ src/controller/AluguelController.java
@@ -8,8 +8,15 @@ package controller;
 import controller.Enumeracoes.EstadoDeConservacao;
 import java.time.Instant;
 import java.sql.Date;
+import java.util.List;
 import model.dao.AluguelDAO;
+import model.dao.ClienteDAO;
+import model.dao.UnidadeDAO;
 import model.vo.Aluguel;
+import model.vo.Cliente;
+import model.vo.Material;
+import model.vo.Unidade;
+import utilitarios.Coleccoes;
 
 
 /**
@@ -18,12 +25,23 @@ import model.vo.Aluguel;
  */
 public class AluguelController {
     private AluguelDAO aluguelDAO;
+    private static List<Aluguel> alugueis;
+    private static long count = 0;
     
     public AluguelController(){
         aluguelDAO = new AluguelDAO();
+        count = 1;
+        atualizarLista();
     }
     
-    public boolean novoAluguel(String codigoCliente, String codigoEmpregado, int codigoUnidade){
+    private void atualizarLista(){
+        if(count > 0){
+            alugueis = aluguelDAO.selecionar();
+        }
+    }
+    
+    private boolean novoAluguel(String codigoCliente, String codigoEmpregado, int codigoUnidade){
+        count++;
         Aluguel aluguel = new Aluguel(
                 codigoCliente,
                 codigoUnidade, 
@@ -31,6 +49,37 @@ public class AluguelController {
                 (Date) java.util.Date.from(Instant.now()),
                 null);
         return aluguelDAO.inserir(aluguel);
+    }
+    
+    private boolean _novoAluguel(int codigoMaterial, String codigoEmpregado, String codigoCliente){
+        UnidadeController unidadeController = new UnidadeController();
+        
+        
+        Unidade unidade = Coleccoes.achar(unidadeController.getUnidadesDisponiveis(), (Unidade u) -> {
+            return u.getCodigo() == codigoMaterial;
+        });
+        
+        if(unidade == null) return false;
+        
+        if(novoAluguel(codigoCliente, codigoEmpregado, unidade.getCodigo())){
+            
+            unidade.setDisponibilidade(Enumeracoes.Disponibilidade.INDISPONIVEL);
+            unidadeController.atualizar(unidade);
+            unidadeController.alocarNaListaDevida(unidade);
+            
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public boolean novoAluguel(int CodigoMaterial, String codigoEmpregado, String nomeCliente, String telefoneCliente, String endereco){
+       ClienteController clienteController = new ClienteController();
+       String codigoCliente = nomeCliente +"_" +telefoneCliente;
+       if(!clienteController.existe(codigoCliente)){
+           clienteController.inserir(codigoCliente, nomeCliente, telefoneCliente, endereco);
+       }
+       return _novoAluguel(CodigoMaterial, codigoEmpregado, codigoCliente);
     }
     
     public boolean adicionarDevolucao(int codigoAluguel, Date data_devolucao, EstadoDeConservacao estadoDeConservacao){
